b25e0e73fb5c947c6440f058b61e621c2f653b7f
Felipe <feventurelli@hotmail.com>
2020-02-22 13:31:30 -0300

    Categorias x Produtos

--- site.php
+++ site.php
@@ -18,6 +18,22 @@ $app->get('/', function() {
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+    $page->setTpl("category", [
+    	'category'=>$category->getValues(),
+    	'products'=>Product::checkList($category->getProducts())
+    ]);
+
+});	
+
 
 
 ?>
